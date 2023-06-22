546192ab5ff24894c059d23e6da63c4d8a5bcd92
Daniel Hernandez <42761673+Rorschach3@users.noreply.github.com>
2023-06-22 02:53:07 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import "./styles.css";
+import { useReducer } from "react"
+import "./styles.css"
 
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT:'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, { type, payload }) {
 
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer,
+  {})
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
@@ -14,21 +26,18 @@ function App() {
       <button>1</button>
       <button>2</button>
       <button>3</button>
+      <button>*</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
+      <button>+</button>
       <button>7</button>
       <button>8</button>
       <button>9</button>
       <button>-</button>
       <button>.</button>
       <button>0</button>
-      <button className="span-two">=</button>
-                     
-                        
-      
-                                                
+      <button className="span-two">=</button>         
     </div>
   )
-};
-export default App
+}
